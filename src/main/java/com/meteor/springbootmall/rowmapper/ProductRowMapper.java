66234fbdc5b640ef8310ce4f22fd220dd61b95c6
DestroyIMeteorI <Destroy5487@gmail.com>
2024-05-20 01:59:45 +0800

    商品功能 - Refactor category，使用 Enum 類型

--- src/main/java/com/meteor/springbootmall/rowmapper/ProductRowMapper.java
+++ src/main/java/com/meteor/springbootmall/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.meteor.springbootmall.rowmapper;
 
+import com.meteor.springbootmall.constant.ProductCategory;
 import com.meteor.springbootmall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -14,7 +15,10 @@ public class ProductRowMapper implements RowMapper<Product> {
 
         product.setProductId(resultSet.getInt("product_id"));
         product.setProductName(resultSet.getString("product_name"));
-        product.setCategory(resultSet.getString("category"));
+
+        // 將資料庫中取出來的 String 字串轉換成 ProductCategory enum
+        product.setCategory(ProductCategory.valueOf(resultSet.getString("category")));
+
         product.setImageUrl(resultSet.getString("image_url"));
         product.setPrice(resultSet.getInt("price"));
         product.setStock(resultSet.getInt("stock"));
