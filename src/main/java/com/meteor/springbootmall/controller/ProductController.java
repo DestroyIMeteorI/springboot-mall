0d1a286f88ab1317f382f58ffa69c763eeeb20f4
DestroyIMeteorI <Destroy5487@gmail.com>
2024-05-20 10:52:38 +0800

    商品功能 - 實作刪除商品

--- src/main/java/com/meteor/springbootmall/controller/ProductController.java
+++ src/main/java/com/meteor/springbootmall/controller/ProductController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -59,5 +60,13 @@ public class ProductController {
         Product updatedProduct = productService.getProductById(productId);
 
         return ResponseEntity.status(HttpStatus.OK).body(updatedProduct);
+    }
+
+    // 刪除商品
+    @DeleteMapping("/products/{productId}")
+    public ResponseEntity<?> deleteProduct(@PathVariable Integer productId) {
+        productService.deleteProductById(productId);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
