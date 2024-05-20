5cd71590ba7d47fb7a761adfdfead4e9bc16abda
DestroyIMeteorI <Destroy5487@gmail.com>
2024-05-20 15:10:01 +0800

    帳號功能 - 註冊新帳號

---
+++ src/main/java/com/meteor/springbootmall/service/UserService.java
@@ -0,0 +1,11 @@
+package com.meteor.springbootmall.service;
+
+import com.meteor.springbootmall.dto.UserRegisterRequest;
+import com.meteor.springbootmall.model.User;
+
+public interface UserService {
+
+    User getUserById(Integer userId);
+
+    Integer register(UserRegisterRequest userRegisterRequest);
+}
